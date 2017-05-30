99fa6bda2f5abe0e1cad2889f092f110d8f6cccf
Chris Cameron <chris@chriscamerondesign.com>
2017-05-30 21:50:50 +0100

    First Commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-app',
+    template: `
+        <h1>Testing</h1>
+    `
+})
+export class AppComponent {
+}
